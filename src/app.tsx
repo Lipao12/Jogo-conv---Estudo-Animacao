46dc0c51e314285835addf0fcf8467e366b23a45
Filipe Mai <f.nunesmai19@gmail.com>
2024-07-24 09:29:58 -0300

    feat: fim da página de inicio, pequena animação para trocar de página

--- src/app.tsx
+++ src/app.tsx
@@ -4,10 +4,23 @@ import { StartPage } from "./pages/initial/start-game";
 
 export function App() {
   const [isStart, setIsStart] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const handleStartGame = () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIsStart(true);
+      setIsAnimating(false);
+    }, 500); // Tempo deve corresponder ao tempo da animação CSS
+  };
   return (
     <div className="flex justify-center items-center min-h-screen bg-space-gradient">
-      <div className="flex justify-center  bg-zinc-900 border border-zinc-900 w-[600px] min-h-[700px] rounded-lg shadow-lg p-6">
-        {isStart ? <Chat /> : <StartPage setStartGame={setIsStart} />}
+      <div
+        className={`flex justify-center  bg-zinc-900 border border-zinc-900 w-[600px] min-h-[700px] rounded-lg shadow-lg p-6 ${
+          isAnimating ? "fade-out" : "fade-in"
+        }`}
+      >
+        {isStart ? <Chat /> : <StartPage setStartGame={handleStartGame} />}
       </div>
     </div>
   );
--- src/pages/initial/start-game.tsx
+++ src/pages/initial/start-game.tsx
@@ -1,18 +1,23 @@
 import { ButtonStart } from "./button-start";
 
 interface StartPageProps {
-  setStartGame: (isStart: boolean) => void;
+  setStartGame: () => void;
 }
 
 export const StartPage = ({ setStartGame }: StartPageProps) => {
-  const handleStartGame = () => {
-    setStartGame(true);
-  };
   return (
-    <div className="flex justify-between flex-col">
-      <div></div>
+    <div className="flex justify-between flex-col p-4">
+      <div className="text-center mb-4">
+        <h1 className="text-4xl font-bold text-white">
+          Bem-vindo à Cosmic Expedition!
+        </h1>
+        <p className="mt-2 text-lg text-gray-300">
+          Prepare-se para uma jornada intergaláctica como nunca antes. Sua
+          aventura começa agora!
+        </p>
+      </div>
       <div>
-        <ButtonStart onClick={handleStartGame} />
+        <ButtonStart onClick={setStartGame} />
       </div>
     </div>
   );
